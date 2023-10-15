7927e7214e5062d46f5aa535dd09a3f6a176b523
nibchee <nirbhaybansal28@gmail.com>
2023-10-15 23:48:47 +0530

    Issue|Concern in generic class

--- src/Zoo.java
+++ src/Zoo.java
@@ -2,7 +2,13 @@ public class Zoo {
     public static void main(String[] args) {
         //As here Cage created with Object class due to which able to have 2 different anmals in same cage but wants smae
         //so generic is used
-     Cage<Monkey> c1=new Cage<>(new Monkey(),new Monkey());
-     Cage<Lion>  c2=new Cage<>(new Lion(),new Lion());
+        Cage<Monkey> c1 = new Cage<>(new Monkey(), new Monkey());
+        Cage<Lion> c2 = new Cage<>(new Lion(), new Lion());
+
+     /*
+    Now here Cage can accept string type objects as well which is not an animal,
+    hence this is issue
+    */
+        Cage<String> c3 = new Cage<>("monkey", "Cat");
     }
 }
