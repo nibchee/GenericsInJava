40af6be165c5e2c3d2d57f5e7f52a592f7a624b1
nibchee <nirbhaybansal28@gmail.com>
2023-10-15 23:44:04 +0530

    Zoo Class Prepared with Object animal Cages but without Generic

--- src/Cage.java
+++ src/Cage.java
@@ -1,9 +1,25 @@
 public class Cage {
-    Animal a1;
-    Animal a2;
+    Object a1;
+    Object a2;
 
-    public Cage(Animal a1, Animal a2) {
+    public Cage(Object a1, Object a2) {
         this.a1 = a1;
+        this.a2 = a2;
+    }
+
+    public Object getA1() {
+        return a1;
+    }
+
+    public void setA1(Object a1) {
+        this.a1 = a1;
+    }
+
+    public Object getA2() {
+        return a2;
+    }
+
+    public void setA2(Object a2) {
         this.a2 = a2;
     }
 }
