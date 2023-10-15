da02930ce037e0d4c19ba5e371a1123881d6d902
nibchee <nirbhaybansal28@gmail.com>
2023-10-15 23:41:27 +0530

    Zoo Basic Design prepared

---
+++ src/Cage.java
@@ -0,0 +1,9 @@
+public class Cage {
+    Animal a1;
+    Animal a2;
+
+    public Cage(Animal a1, Animal a2) {
+        this.a1 = a1;
+        this.a2 = a2;
+    }
+}
---
+++ src/Zoo.java
@@ -0,0 +1,5 @@
+public class Zoo {
+    public static void main(String[] args) {
+     Cage c1=new Cage(new Monkey(),new Lion());
+    }
+}
